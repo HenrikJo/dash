b7d0e740cfe9e9978f3cff9de8c055905d72f299
Henrik <noreply@fake.com>
2026-01-15 22:48:41 +0100

    Move vehicle creation to separate function

--- vehicle.c
+++ vehicle.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define debug 0
 
@@ -86,7 +87,7 @@ float rpm_to_rev(float rpm, unsigned int gear)
 
 float calculate_acceleration(float torque, float mass, float deltatime)
 {
-    float acceleration = (torque / mass) * deltatime;
+    return (torque / mass) * deltatime;
 }
 
 float rads_to_rpm(float rads)
@@ -240,81 +241,89 @@ void update_speed_separately(struct vehicle_info *self, float deltatime)
     self->vehicle_speed += (force / mass) * deltatime;
 }
 
-
-
-int main(void) 
+struct vehicle_info *create_vehicle(void)
 {
-    struct vehicle_info yamaha_fjr_1300;
-    yamaha_fjr_1300.gears = sizeof(gear_ratio_yamaha_fjr_1300) / sizeof(float);
-    yamaha_fjr_1300.gear_ratio = gear_ratio_yamaha_fjr_1300;
+    struct vehicle_info *yamaha_fjr_1300 = malloc(sizeof(struct vehicle_info));
+    assert(yamaha_fjr_1300 != NULL);
 
-    yamaha_fjr_1300.speed_torque_values = sizeof(rpm_torque_yamaha_fjr_1300) / sizeof(rpm_torque_yamaha_fjr_1300[0]);
-    yamaha_fjr_1300.torque_at_rpm = rpm_torque_yamaha_fjr_1300;
+    yamaha_fjr_1300->gears = sizeof(gear_ratio_yamaha_fjr_1300) / sizeof(float);
+    yamaha_fjr_1300->gear_ratio = gear_ratio_yamaha_fjr_1300;
 
-    yamaha_fjr_1300.rotor_mass = 1.0f;
-    yamaha_fjr_1300.rotor_radius = 0.1f;
-    yamaha_fjr_1300.rotor_drag_torque = -10.0f;
-    yamaha_fjr_1300.idle = 1400.0f;
+    yamaha_fjr_1300->speed_torque_values = sizeof(rpm_torque_yamaha_fjr_1300) / sizeof(rpm_torque_yamaha_fjr_1300[0]);
+    yamaha_fjr_1300->torque_at_rpm = rpm_torque_yamaha_fjr_1300;
 
-    yamaha_fjr_1300.primary_gear_reduction = 1.563f;
-    yamaha_fjr_1300.final_gear_reduction = 2.773f;
+    yamaha_fjr_1300->rotor_mass = 1.0f;
+    yamaha_fjr_1300->rotor_radius = 0.1f;
+    yamaha_fjr_1300->rotor_drag_torque = -10.0f;
+    yamaha_fjr_1300->idle = 1400.0f;
 
-    yamaha_fjr_1300.selected_gear = 1;
+    yamaha_fjr_1300->primary_gear_reduction = 1.563f;
+    yamaha_fjr_1300->final_gear_reduction = 2.773f;
+
+    yamaha_fjr_1300->selected_gear = 1;
 
     /* Wheel size 180/55ZR17 */
-    yamaha_fjr_1300.wheel_diameter = (17.0f * 25.4 + 55.0f) / 1000.0f;
-    yamaha_fjr_1300.body_mass = 250.0f;
+    yamaha_fjr_1300->wheel_diameter = (17.0f * 25.4 + 55.0f) / 1000.0f;
+    yamaha_fjr_1300->body_mass = 250.0f;
 
-    printf("Gears: %d\n", yamaha_fjr_1300.gears);
-    for (int i = 0; i < yamaha_fjr_1300.gears; i++) {
-        yamaha_fjr_1300.selected_gear = i+1;
-        printf("Ratio at gear %d: %f\n", yamaha_fjr_1300.selected_gear - 1, yamaha_fjr_1300.gear_ratio[i]);
+    return yamaha_fjr_1300;
+}
+
+int main(void)
+{
+    struct vehicle_info *yamaha_fjr_1300 = create_vehicle();
+
+
+    printf("Gears: %d\n", yamaha_fjr_1300->gears);
+    for (int i = 0; i < yamaha_fjr_1300->gears; i++) {
+        yamaha_fjr_1300->selected_gear = i+1;
+        printf("Ratio at gear %d: %f\n", yamaha_fjr_1300->selected_gear - 1, yamaha_fjr_1300->gear_ratio[i]);
     }
 
     for (float rpm = 0.0f; rpm < 10000.0f; rpm += 100.0f) {
-        float torque = torque_at_rpm(&yamaha_fjr_1300, rpm);
+        float torque = torque_at_rpm(yamaha_fjr_1300, rpm);
         printf("Engine RPM: %f rpm, torque: %f Nm\n", rpm, torque);
     }
 
     /* Check wheel torque at peak torque rpm */
     float rpm = 6500.0f;
-    for (unsigned int gear = 0; gear <= yamaha_fjr_1300.gears; gear++) {
-        float wheel_torque = get_wheel_torque(&yamaha_fjr_1300, rpm, gear);
-        float vehicle_motor_force = wheel_torque / yamaha_fjr_1300.wheel_diameter / 2.0f;
+    for (unsigned int gear = 0; gear <= yamaha_fjr_1300->gears; gear++) {
+        float wheel_torque = get_wheel_torque(yamaha_fjr_1300, rpm, gear);
+        float vehicle_motor_force = wheel_torque / yamaha_fjr_1300->wheel_diameter / 2.0f;
         printf("Engine RPM: %f rpm, wheel torque: %f Nm, force: %fN\n", rpm, wheel_torque, vehicle_motor_force);
     }
 
     /* Check rpm and speed when disengaged clutch */
-    yamaha_fjr_1300.vehicle_speed = 100.0f;
-    yamaha_fjr_1300.engine_rpm = 2000.0f;
-    yamaha_fjr_1300.throttle = 1.0f;
+    yamaha_fjr_1300->vehicle_speed = 100.0f;
+    yamaha_fjr_1300->engine_rpm = 2000.0f;
+    yamaha_fjr_1300->throttle = 1.0f;
     for (int i = 0; i < 50; i++) {
-        printf("Speed %f, rpm %f\n", yamaha_fjr_1300.vehicle_speed, yamaha_fjr_1300.engine_rpm);
-        update_speed_separately(&yamaha_fjr_1300, 0.1f);
+        printf("Speed %f, rpm %f\n", yamaha_fjr_1300->vehicle_speed, yamaha_fjr_1300->engine_rpm);
+        update_speed_separately(yamaha_fjr_1300, 0.1f);
     }
 
-    yamaha_fjr_1300.throttle = 0.0f;
+    yamaha_fjr_1300->throttle = 0.0f;
     for (int i = 0; i < 50; i++) {
-        printf("Speed %f, rpm %f\n", yamaha_fjr_1300.vehicle_speed, yamaha_fjr_1300.engine_rpm);
-        update_speed_separately(&yamaha_fjr_1300, 0.1f);
+        printf("Speed %f, rpm %f\n", yamaha_fjr_1300->vehicle_speed, yamaha_fjr_1300->engine_rpm);
+        update_speed_separately(yamaha_fjr_1300, 0.1f);
     }
 
     /* Calculate speed going from disengaged to engaged clutch */
-    yamaha_fjr_1300.selected_gear = 1;
-    calc_engaged_clutch_speeds(&yamaha_fjr_1300);
+    yamaha_fjr_1300->selected_gear = 1;
+    calc_engaged_clutch_speeds(yamaha_fjr_1300);
 
     printf("\nSimulate no throttle\n");
-    yamaha_fjr_1300.throttle = 0.0f;
+    yamaha_fjr_1300->throttle = 0.0f;
     for (int i = 0; i < 100; i++) {
-        printf("Speed %f, rpm %f\n", yamaha_fjr_1300.vehicle_speed, yamaha_fjr_1300.engine_rpm);
-        update_speed_combined(&yamaha_fjr_1300, 0.1f);
+        printf("Speed %f, rpm %f\n", yamaha_fjr_1300->vehicle_speed, yamaha_fjr_1300->engine_rpm);
+        update_speed_combined(yamaha_fjr_1300, 0.1f);
     }
 
     printf("\nSimulate full throttle\n");
-    yamaha_fjr_1300.throttle = 1.0f;
+    yamaha_fjr_1300->throttle = 1.0f;
     for (int i = 0; i < 50; i++) {
-        printf("Speed %f, rpm %f\n", yamaha_fjr_1300.vehicle_speed, yamaha_fjr_1300.engine_rpm);
-        update_speed_combined(&yamaha_fjr_1300, 0.1f);
+        printf("Speed %f, rpm %f\n", yamaha_fjr_1300->vehicle_speed, yamaha_fjr_1300->engine_rpm);
+        update_speed_combined(yamaha_fjr_1300, 0.1f);
     }
 
 	return 0;
