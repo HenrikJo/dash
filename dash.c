f94ea748c01fbbd9081617cb2c149145cd53cc36
Henrik <6512054+HenrikJo@users.noreply.github.com>
2026-08-10 22:32:15 +0200

    Fix syntax

--- dash.c
+++ dash.c
@@ -1,29 +1,32 @@
 struct rpm_torque {
     float rpm;
     float torque;
-}
+};
 
 struct rpm_torque rpm_tq[] = {
-    { .rpm = 0, .torque = 0 }
-    { .rpm = 1600, .torque = 45 }
-    { .rpm = 2000, .torque = 62 }
-    { .rpm = 3000, .torque = 73 }
-    { .rpm = 4000, .torque = 80 }
-    { .rpm = 5000, .torque = 85 }
-    { .rpm = 6000, .torque = 85 }
-    { .rpm = 6500, .torque = 90 }
-    { .rpm = 7000, .torque = 89 }
-    { .rpm = 8000, .torque = 80 }
-    { .rpm = 9000, .torque = 65 }
+    { .rpm = 0, .torque = 0 },
+    { .rpm = 1600, .torque = 45 },
+    { .rpm = 2000, .torque = 62 },
+    { .rpm = 3000, .torque = 73 },
+    { .rpm = 4000, .torque = 80 },
+    { .rpm = 5000, .torque = 85 },
+    { .rpm = 6000, .torque = 85 },
+    { .rpm = 6500, .torque = 90 },
+    { .rpm = 7000, .torque = 89 },
+    { .rpm = 8000, .torque = 80 },
+    { .rpm = 9000, .torque = 65 },
     { .rpm = 9500, .torque = 0 }
-}
+};
+
 float gear_ratio[] = { 2.5, 1.722, 1.35, 1.111, 0.962, 0.846 };
 
-float rpm_to_rev(float rpm, uint8_t gear)
-{
 
+float rpm_to_rev(float rpm, unsigned int gear)
+{
+    return 0.0f;
 }
 
-int main(void) {
-	
+int main(void) 
+{
+	return 0;
 }
